f320849547addd0ec2632a65e714c4aa6467d6e6
Prashant Sengar <prashantsengar812@gmail.com>
2020-10-22 15:09:04 +0530

    updated API

--- Controllers/retrieveController.cs
+++ Controllers/retrieveController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 namespace Stockpoint.Controllers
 {
-[Route("api/[controller]")]
+[Route("[controller]")]
     [ApiController]
     public class retrieveController : ControllerBase
     {
@@ -17,38 +17,67 @@ namespace Stockpoint.Controllers
             this.Context = context;
             this.categoryContext=categoryContext;
         }
-
-        [HttpGet("all")]
-        public ActionResult<IEnumerable<Inventory>> all()
-        {
-            return this.Context.inventory.ToList();
- 
-        }
-        [HttpGet("search")]
-        public ActionResult<IEnumerable<Inventory>> search(string key){
-            List<Inventory> myList=new List<Inventory>();
-            Dictionary<int,string> mydict=new Dictionary<int, string>();
-            foreach(Category obj in this.categoryContext.category){
-                mydict[obj.id]=obj.category_name;
+        public bool compareDates(string start,string end,string cur){
+            string[] startDate=start.Split("/");
+            string[] endDate=end.Split("/");
+            string[] curDate=cur.Split("/");
+            //System.Diagnostics.Debug.WriteLine(startDate[2]+" is the start year")
+            for(int i=2;i>=0;i--){
+                
+                if(int.Parse(startDate[i])<=int.Parse(curDate[i]) && int.Parse(endDate[i])>=int.Parse(curDate[i])){
+                    continue;
+                }
+                else{
+                    return false;
+                }
             }
-            foreach(Inventory obj in this.Context.inventory){
-                string[] str=new string[9];
-                str[0]=(obj.id).ToString();
-                str[1]=obj.invoice_id.ToString();
-                int cat_id=obj.category_id;
-                str[2]=mydict[cat_id]; 
-                str[3]=(string)obj.item_name;
-                str[4]=(string)obj.item_model;
-                str[5]=obj.quantity.ToString();
-                str[6]=obj.price_per_unit.ToString();
-                str[7]=(string)obj.date_of_purchase;
-                foreach(string st in str){
-                    if (st==key){
-                        myList.Add(obj);
-                        break;
+
+            return true;
+        }
+        // [HttpGet("all")]
+        // public ActionResult<IEnumerable<Inventory>> all()
+        // {
+        //     return this.Context.inventory.ToList();
+ 
+        // }
+        [HttpGet]
+        public ActionResult<IEnumerable<Inventory>> search(string inv_id,string category,string startDate,string endDate){
+            List<Inventory> myList=new List<Inventory>();
+            Dictionary<int,string> myDict=new Dictionary<int,string>();
+            foreach(Category cat in this.categoryContext.category){
+                myDict[cat.id]=cat.category_name;
+            }
+            
+            bool[] flag=new bool[4];
+            flag[0]=inv_id==null?false:true;
+            flag[1]=category==null?false:true;
+            flag[2]=startDate==null?false:true;
+            flag[3]=endDate==null?false:true;
+            if(flag[3]==false){
+                endDate=startDate;
+            }
+            foreach(Inventory ob in this.Context.inventory){
+                string cat_name=myDict[ob.category_id];
+                
+                if(flag[0]==true && ob.invoice_id.ToString()!=inv_id){
+                    continue;
+                }
+                else if(flag[1]==true && cat_name!=category){
+                    continue;
+                }
+                // else if(flag[2]==true && !(startDate.CompareTo(ob.date_of_purchase)<=0) && endDate.CompareTo(ob.date_of_purchase)>=0){
+                //     continue;
+                // }
+                else{
+                    if(flag[2]!=false){
+                    if(compareDates(startDate,endDate,ob.date_of_purchase)==true){
+                    myList.Add(ob);
+                    }
+                    }
+                    else{
+                        myList.Add(ob);
                     }
                 }
-         
             }
             return myList;
         }
