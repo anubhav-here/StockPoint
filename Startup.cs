a9d36b40a08c75fc01276c21d56cf7c39f576261
Deep <Anubhav.Deep@shell.com>
2020-10-23 00:14:34 +0530

    Fixed Cors Errors for the API

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,13 @@ namespace Stockpoint
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+        {
+        options.AddPolicy("CorsPolicy",
+            builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
             string connectionstring = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<InventoryContext>(x => x.UseSqlServer(connectionstring));
             services.AddDbContext<CategoryContext>(x => x.UseSqlServer(connectionstring));
@@ -36,6 +43,7 @@ namespace Stockpoint
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("CorsPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
